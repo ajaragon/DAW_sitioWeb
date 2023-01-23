d5e8bf015326cf62948e72df87c6c84ef72bac7c
ajaragon <114513177+ajaragon@users.noreply.github.com>
2023-01-23 16:24:23 +0100

    Add files via upload

---
+++ DAW_paginaWeb.php
@@ -0,0 +1,32 @@
+<?php
+
+function carga($clase)
+{
+
+    require "$clase.php";
+
+}//fin de la función
+
+header("location:areaUsuario.php");
+
+spl_autoload_register("carga");
+
+?>
+
+<html>
+    <head>
+        <title>Formulario</title>
+    </head>
+    <body>
+        <fieldset>
+            <legend>Formulario</legend>
+            <form>
+                <label>NOMBRE: <input type="text" name="nombre" id=""/></label>
+                <label>APELLIDO: <input type="text" name="apellido" id=""/></label>
+                <label>DNI: <input type="text" name="dni" id=""/></label>
+                <input type="submit" value="Acceder" name="submit" id=""/></label>
+            </form>
+        </fieldset>
+    </body>
+</html>
+
---
+++ areaUsuario.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <title>Área del usuario</title>
+    </head>
+    <body>
+        <fieldset>
+            <legend>Área del usuario</legend>
+            <h3>BIENVENIDO</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Etiam id rutrum neque, at mattis orci.
+                Nunc id leo libero.
+                Nunc in lacus finibus, dignissim tellus vel, sodales elit.
+                Pellentesque id varius purus.
+                Phasellus sapien nibh, hendrerit ut neque id, condimentum sollicitudin dui.
+                Donec at enim convallis, faucibus tellus at, facilisis est.
+                Maecenas felis nunc, elementum nec tellus nec, egestas viverra felis.
+                Nam dapibus viverra finibus. Proin eu ipsum vitae nulla vulputate iaculis.
+                Maecenas quis gravida ante.
+                Maecenas dolor erat, vehicula eu enim non, volutpat ullamcorper elit.
+                Quisque a pretium nisi, et suscipit elit.
+                Mauris nec lectus odio.
+                Ut varius turpis metus, eget lacinia tortor egestas sed. </p>
+        </fieldset>
+    </body>
+</html>
